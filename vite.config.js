0ed9d15f6b2c16a27bc69b18c46ac6365a673d0d
LI Xueyu <susie.lxyu@gmail.com>
2024-05-21 00:11:58 +0800

    restore vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "./vue-sample/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vue-sample/" : "/",
   // build: {
   //   outDir: 'public'
   // },
