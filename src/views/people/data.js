2fd4bbdc1db84f08a2101f5e9b99da17768b6be7
yuan <1044375716@qq.com>
2024-07-23 14:58:48 +0800

    commit on main

--- src/views/people/data.js
+++ src/views/people/data.js
@@ -8,12 +8,12 @@ export const staffGroup = [
     groupTitle: 'Group 1',
     members: [
       {
-        name: "xxx 教授",
-        desc: "Head & Chair Professor",
-        content: "Ohter introduction",
-        email: 'xxx@xxx.edu.hk',
+        name: "董长宇 教授",
+        desc: "Chair Professor",
+        content: "广州大学人工智能与区块链研究院的教育部长江学者特聘教授，纽卡斯尔大学访问教授和艾伦·图灵研究所的图灵研究员。研究主要集中在网络安全领域，如应用密码学、数据隐私、人工智能安全和信任管理等。最近的工作主要关注设计实用的安全计算协议，并应用于大规模隐私保护数据处理。应用领域包括隐私保护的医疗数据分析、安全云计算、隐私保护的数据挖掘和私有机器学习。/n前纽卡斯尔大学计算机学院的高级讲师。在加入纽卡斯尔大学之前曾是斯特拉斯克莱德大学的讲师，在此之前是伦敦帝国理工学院计算机系的博士后研究员。分别于2009年和2005年在帝国理工学院获得博士和硕士学位。于1999年在复旦大学获得学士学位。在中国也做过几年网络工程师",
+        email: 'changyu.dong AT gmail.com',
         url: 'https://changyudong.site',
-        location: 'some location',
+        location: '广东省广州市',
         imageUrl: 'avater.png'
       },
       {
